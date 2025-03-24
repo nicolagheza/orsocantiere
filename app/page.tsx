85ce3dfc2ad0e089e73f82d04595e82c7be1bfc2
nicolagheza <ghzncl@gmail.com>
2025-03-24 01:32:30 +0100

    Changes demo homepage when logged out

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ export default async function Home() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h1>EDILMABAC - ERP by ViaTecnologia.it x nicode.</h1>
+        <h1>EDILMABAC - ERP</h1>
+        <h2>A solution by nicode. x ViaTecnologia.it</h2>
       </main>
     </>
   );
